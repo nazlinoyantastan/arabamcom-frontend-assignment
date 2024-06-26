9532280c2178dfd4e0c41ab28b5f8991c2d69fe0
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-06-26 15:45:37 +0300

    swagger

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8081/http://sandbox.arabamd.com/api/v1',
+  baseURL: '/http://sandbox.arabamd.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
