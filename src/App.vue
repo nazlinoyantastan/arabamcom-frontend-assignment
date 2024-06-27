d58ccd1a4f6a46e22208107bd7369f5e141d64ea
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-06-27 08:56:38 +0300

    240627-v1

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,5 @@ export default {
 </script>
 
 <style>
-/* İstediğiniz stil düzenlemelerini buraya ekleyebilirsiniz */
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Router'ı dahil edin
+
+import './main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).use(router).mount('#app');
+
