997e00477f7fe3f5c2c68d8e3343ec6b9477dd9f
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-06-29 13:57:05 +0300

    240629-v3

--- src/views/ListingPage.vue
+++ src/views/ListingPage.vue
@@ -1,49 +1,80 @@
 <template>
-    <div class="container">
-      <h1>Vitrin</h1>
-  
-      <!-- İlan sayısını seçmek için dropdown -->
-      <div class="filter">
-        <label for="advertCount">İlan Sayısı:</label>
-        <select id="advertCount" v-model="selectedCount" @change="fetchListings">
-          <option value="20">20</option>
-          <option value="50">50</option>
-        </select>
-      </div>
+  <div class="container">
+    <h1>Vitrin</h1>
 
-      
-  
-      <table class="ad-table">
-        <thead>
-          <tr>
-            <th colspan="2" class="model-header">Model</th>
-            <th>İlan Başlığı</th>
-            <th class="year-header" @click="setSort('year')">
-                <span>Yıl</span>                    
-            </th>
-            <th>Kilometre</th>
-            <th>Renk</th>
-            <th class="price-header" @click="setSort('price')">Fiyat</th>
-            <th class="date-header" @click="setSort('date')">Tarih</th>
-            <th>İl / İlçe</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="ad in sortedAdverts" :key="ad.id" @click="goToDetail(ad.id)" class="ad-item">
-            <td><img :src="formatImageUrl(ad.photo)" alt="Araba Resmi" class="ad-image"></td>
-            <td class="left-align">{{ ad.modelName }}</td>
-            <td class="left-align">{{ ad.title }}</td>
-            <td>{{ getProperty(ad.properties, 'year') }}</td>
-            <td>{{ formatNumber(getProperty(ad.properties, 'km')) }}</td>
-            <td>{{ getProperty(ad.properties, 'color') }}</td>
-            <td class="price">{{ formatNumber(ad.price) }} ₺</td>
-            <td class="date">{{ ad.dateFormatted}}</td>
-            <td class="location">{{ getLocation(ad.location) }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <!-- İlan sayısını seçmek için dropdown -->
+    <div class="filter">
+      <label for="advertCount">İlan Sayısı:</label>
+      <select id="advertCount" v-model="selectedCount" @change="fetchListings">
+        <option value="20">20</option>
+        <option value="50">50</option>
+      </select>
     </div>
+
+    <!-- Filtreleme formu -->
+    <div class="filters">
+      <div>
+        <label for="minPrice">Min Fiyat:</label>
+        <input type="text" id="minPrice" :value="filters.minPrice" @input="formatPriceInput('minPrice', $event)">
+      </div>
+      <div>
+        <label for="maxPrice">Max Fiyat:</label>
+        <input type="text" id="maxPrice" :value="filters.maxPrice" @input="formatPriceInput('maxPrice', $event)">
+      </div>
+      <div>
+        <label for="minYear">Min Yıl:</label>
+        <input type="number" id="minYear" v-model.number="filters.minYear" @input="applyFilters">
+      </div>
+      <div>
+        <label for="maxYear">Max Yıl:</label>
+        <input type="number" id="maxYear" v-model.number="filters.maxYear" @input="applyFilters">
+      </div>
+      <div>
+        <label for="color">Renk:</label>
+        <input type="text" id="color" v-model="filters.color" @input="applyFilters">
+      </div>
+      <div>
+        <label for="minDate">Min Tarih:</label>
+        <input type="date" id="minDate" v-model="filters.minDate" @input="applyFilters">
+      </div>
+      <div>
+        <label for="maxDate">Max Tarih:</label>
+        <input type="date" id="maxDate" v-model="filters.maxDate" @input="applyFilters">
+      </div>
+    </div>
+
+    <table class="ad-table">
+      <thead>
+        <tr>
+          <th colspan="2" class="model-header">Model</th>
+          <th>İlan Başlığı</th>
+          <th class="year-header" @click="setSort('year')">
+            <span>Yıl</span>
+          </th>
+          <th>Kilometre</th>
+          <th>Renk</th>
+          <th class="price-header" @click="setSort('price')">Fiyat</th>
+          <th class="date-header" @click="setSort('date')">Tarih</th>
+          <th>İl / İlçe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ad in sortedAdverts" :key="ad.id" @click="goToDetail(ad.id)" class="ad-item">
+          <td><img :src="formatImageUrl(ad.photo)" alt="Araba Resmi" class="ad-image"></td>
+          <td class="left-align">{{ ad.modelName }}</td>
+          <td class="left-align">{{ ad.title }}</td>
+          <td>{{ getProperty(ad.properties, 'year') }}</td>
+          <td>{{ formatNumber(getProperty(ad.properties, 'km')) }}</td>
+          <td>{{ getProperty(ad.properties, 'color') }}</td>
+          <td class="price">{{ formatNumber(ad.price) }} ₺</td>
+          <td class="date">{{ ad.dateFormatted }}</td>
+          <td class="location">{{ getLocation(ad.location) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
   
 <script>
 import api from '../api';
@@ -52,68 +83,87 @@ export default {
   data() {
     return {
       adverts: [],
-      selectedCount: 20, // Başlangıçta 20 ilan seçili
-      sortType: 'date', // İlk olarak tarihe göre sıralama
-      sortOrder: 'desc' // Yeni eklenenden eskiye doğru
-      
+      selectedCount: 20,
+      sortType: 'date',
+      sortOrder: 'desc',
+      filters: {
+        minPrice: '',
+        maxPrice: '',
+        minYear: null,
+        maxYear: null,
+        color: '',
+        minDate: null,
+        maxDate: null,
+      },
     };
   },
   computed: {
     sortedAdverts() {
-    let sorted = this.adverts.slice();
+      let filtered = this.adverts.filter(ad => {
+        const price = this.parseFormattedNumber(ad.price);
+        const year = parseInt(this.getProperty(ad.properties, 'year'));
+        const color = this.getProperty(ad.properties, 'color');
+        const date = new Date(ad.date.split('.').reverse().join('-'));
 
-    switch (this.sortType) {
-      case 'date':
-        sorted.sort(this.sortByDate);
-        break;
-      case 'price':
-        sorted.sort(this.sortByPrice);
-        break;
-      case 'year':
-        sorted.sort(this.sortByYear);
-        break;
-      default:
-        break;
+        return (
+          (!this.filters.minPrice || price >= this.parseFormattedNumber(this.filters.minPrice)) &&
+          (!this.filters.maxPrice || price <= this.parseFormattedNumber(this.filters.maxPrice)) &&
+          (!this.filters.minYear || year >= this.filters.minYear) &&
+          (!this.filters.maxYear || year <= this.filters.maxYear) &&
+          (!this.filters.color || color.toLowerCase().includes(this.filters.color.toLowerCase())) &&
+          (!this.filters.minDate || date >= new Date(this.filters.minDate)) &&
+          (!this.filters.maxDate || date <= new Date(this.filters.maxDate))
+        );
+      });
+
+      switch (this.sortType) {
+        case 'date':
+          filtered.sort(this.sortByDate);
+          break;
+        case 'price':
+          filtered.sort(this.sortByPrice);
+          break;
+        case 'year':
+          filtered.sort(this.sortByYear);
+          break;
+        default:
+          break;
+      }
+
+      return this.sortOrder === 'asc' ? filtered : filtered.reverse();
     }
-
-    return this.sortOrder === 'asc' ? sorted : sorted.reverse();
-  }
   },
   methods: {
     fetchListings() {
-        const params = {
-    take: this.selectedCount,
-   };
-
+      const params = {
+        take: this.selectedCount,
+      };
 
       api.getListings(params)
         .then(response => {
-          console.log('API yanıtı:', response.data);
           this.adverts = response.data;
         })
         .catch(error => {
           console.error('İlanlar alınırken hata oluştu:', error);
         });
     },
-
+    applyFilters() {
+      this.$forceUpdate(); // computed property'i tetiklemek için
+    },
     setSort(type) {
-    if (this.sortType === type) {
-      // Aynı sütuna tıklanırsa sıralama sırasını değiştir
-      this.sortOrder = (this.sortOrder === 'asc') ? 'desc' : 'asc';
-    } else {
-      // Farklı bir sütuna tıklanırsa sıralama türünü değiştir ve varsayılan olarak artan sıralama yap
-      this.sortType = type;
-      this.sortOrder = 'asc';
-    }
-  },
-
-    
+      if (this.sortType === type) {
+        this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+      } else {
+        this.sortType = type;
+        this.sortOrder = 'asc';
+      }
+    },
     goToDetail(id) {
       this.$router.push({ name: 'DetailPage', params: { id } });
     },
     formatImageUrl(url) {
       if (url) {
-        return url.replace('{0}', '120x90'); // URL'deki yer tutucuyu doğru çözünürlükle değiştir
+        return url.replace('{0}', '120x90');
       }
       return '';
     },
@@ -123,9 +173,9 @@ export default {
     },
     getLocation(location) {
       if (!location) {
-        return ''; // location null veya undefined ise boş string döndür
+        return '';
       }
-      return `${location.cityName}\n${location.townName}`; // İl ve ilçe bilgilerini alt alta yaz
+      return `${location.cityName}\n${location.townName}`;
     },
     formatNumber(value) {
       if (!value) return '';
@@ -138,22 +188,31 @@ export default {
       return value;
     },
     sortByDate(a, b) {
-    const dateA = new Date(a.date.split('.').reverse().join('-'));
-    const dateB = new Date(b.date.split('.').reverse().join('-'));
-    return dateA - dateB;
-  },
-  sortByPrice(a, b) {
-    return this.parseFormattedNumber(a.price) - this.parseFormattedNumber(b.price);
-  },
-  sortByYear(a, b) {
-    return this.getProperty(a.properties, 'year') - this.getProperty(b.properties, 'year');
-  }
-
+      const dateA = new Date(a.date.split('.').reverse().join('-'));
+      const dateB = new Date(b.date.split('.').reverse().join('-'));
+      return dateA - dateB;
+    },
+    sortByPrice(a, b) {
+      return this.parseFormattedNumber(a.price) - this.parseFormattedNumber(b.price);
+    },
+    sortByYear(a, b) {
+      return this.getProperty(a.properties, 'year') - this.getProperty(b.properties, 'year');
+    },
+    formatPriceInput(field, event) {
+      let value = event.target.value;
+      // Remove all non-numeric characters
+      value = value.replace(/[^\d]/g, '');
+      // Add dots every three digits
+      value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+      this.filters[field] = value;
+      this.applyFilters();
+    }
   },
   created() {
     this.fetchListings();
   }
 };
+
 </script>
 
 <style>
@@ -161,7 +220,7 @@ export default {
   max-width: 1500px;
   margin: 0 auto;
   padding: 20px;
-  background-color: #f7f7f7 ;
+  background-color: #f7f7f7;
 }
 
 .filter, .filters {
@@ -170,7 +229,7 @@ export default {
 
 .filters {
   display: flex;
-  justify-content:center;
+  justify-content: center;
   gap: 10px;
   flex-wrap: wrap;
 }
@@ -179,35 +238,40 @@ export default {
   display: block;
 }
 
+.filters input[type="text"],
+.filters input[type="number"],
+.filters input[type="date"] {
+  padding: 5px;
+  font-size: 14px;
+  width: 100%;
+}
+
 .ad-table {
   width: 100%;
   border-collapse: collapse;
 }
 
-.ad-table th{
-    background-color: #ffffff;
+.ad-table th {
+  background-color: #ffffff;
 }
 
 .ad-table th, .ad-table td {
   border: 1px solid #ffffff;
   padding: 10px;
-  text-align: center; /* Yatayda ortalama */
-  vertical-align: middle; /* Dikeyde ortalama */
+  text-align: center;
+  vertical-align: middle;
 }
 
 .ad-table th.model-header {
-   
-  text-align: center; /* Başlıkları ortalama */
+  text-align: center;
 }
-
-
 
 .ad-table th.price-header {
-  width: 150px; /* Fiyat kutucuğunu genişletme */
-  cursor: pointer; /* Başlıkların tıklanabilir olduğunu göstermek için */
+  width: 150px;
+  cursor: pointer;
 }
 .ad-table th.year-header, th.date-header {
-  cursor: pointer; /* Başlıkların tıklanabilir olduğunu göstermek için */
+  cursor: pointer;
 }
 .ad-table th.year-header:hover {
   color: red;
@@ -219,7 +283,7 @@ export default {
   color: red;
 }
 .ad-table th.left-align, .ad-table td.left-align {
-  text-align: left; /* Satır başından başlama */
+  text-align: left;
 }
 
 .ad-table th {
@@ -235,7 +299,7 @@ export default {
 }
 
 .ad-item:hover {
-  background-color: #ffe6e6; /* Hafif kırmızımsı arka plan rengi */
+  background-color: #ffe6e6;
 }
 
 .ad-image {
@@ -246,16 +310,14 @@ export default {
 
 .price {
   color: red;
-  font-weight:bold;
-  white-space: nowrap; /* Tek satırda gösterim */
+  font-weight: bold;
+  white-space: nowrap;
 }
 .location {
-  white-space: pre-line; /* İl ve ilçe bilgisini alt alta yazmak için */
+  white-space: pre-line;
 }
 .date {
-  white-space: pre-line; 
+  white-space: pre-line;
 }
-
-
 
 </style>
